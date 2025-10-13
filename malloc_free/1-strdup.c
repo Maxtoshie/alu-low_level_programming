4f07bf4d4f356681bc57fc909e4807614b76e10e
Maxtoshie <maaxwelokoth@gmail.com>
2025-10-14 00:12:27 +0300

    Removes strlen

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,27 +1,34 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _strdup - a function that returns a pointer to a newly allocated
- * space in memory, which contains a copy of the string given as a parameter.
- * @str: the string to be duplicated
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: string to duplicate
  *
- * Return: pointer to duplicated string.
+ * Return: pointer to duplicated string, or NULL if insufficient memory
+ * or str is NULL
  */
-
 char *_strdup(char *str)
 {
-	int i, len;
-	char *s;
+	int i, len = 0;
+	char *dup;
 
 	if (str == NULL)
 		return (NULL);
-	len = strlen(str);
-	s = malloc(len + 1);
-	if (s == NULL)
+
+	/* calculate length of str manually */
+	while (str[len] != '\0')
+		len++;
+
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
 		return (NULL);
-	for (i = 0; i <= len; i++)
-		s[i] = str[i];
-	return (s);
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+
+	dup[len] = '\0';
+
+	return (dup);
 }
